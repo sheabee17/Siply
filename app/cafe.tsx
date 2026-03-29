bdaebf168ee763c05ebe77c08a12090d5d8874fb
Claire King <cking6@trinity.edu>
2026-03-28 22:01:21 -0500

    coffee shop profile

---
+++ app/cafe.tsx
@@ -0,0 +1,14 @@
+import { ScrollView } from 'react-native';
+import CoffeeHeader from '@/components/CoffeeHeader';
+import CoffeeStats from '@/components/CoffeeStats';
+import CoffeeTabs from '@/components/CoffeeTabs';
+
+export default function CafeScreen() {
+  return (
+    <ScrollView>
+      <CoffeeHeader />
+      <CoffeeStats />
+      <CoffeeTabs />
+    </ScrollView>
+  );
+}
